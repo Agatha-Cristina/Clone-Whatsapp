2c2881cb56686fb75704924605296783ade9a807
Agatha <agatha.cristina0507@gmail.com>
2020-09-09 01:08:46 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './App.css';
 
 export default () => {
   return (
-    <div>
-      Esta funcionando
+    <div className="app-window">
+      <div className="sidebar">
+        
+        <header>
+          ...
+        </header>
+
+        <div className="search">
+          ...
+        </div>
+          ...
+        <div className="chatlist">
+          ...
+        </div>
+
+
+      </div>
+      <div className="contentarea">
+        ...
+      </div>
     </div>
   );
 }
